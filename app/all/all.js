7040b72a91fa08532931a86c97a7899cfec5a1d3
Rylan Lambertsen <xxryloxx@gmail.com>
2017-03-27 15:33:46 -0400

    backup push

--- app/all/all.js
+++ app/all/all.js
@@ -8,8 +8,8 @@ app.config(['$routeProvider', function($routeProvider) {
 }])
 
 app.controller('allCtrl', ['$scope', '$http', function($scope, $http) {
-	var url = 'http://iwg-prod-web-interview.azurewebsites.net/stem/v1/funds'; 
-    $http.get(url).then(function(response) {
+	var url = 'http://iwg-prod-web-interview.azurewebsites.net/stem/v1/funds'; // url to get dunds info
+    $http.get(url).then(function(response) { // get all funds info
         console.log(response);
         $scope.all = response.data;
     });
--- app/detailed/detailed.js
+++ app/detailed/detailed.js
@@ -10,14 +10,14 @@ detailed.config(['$routeProvider', function($routeProvider) {
 detailed.controller('View2Ctrl', ['$scope', '$location', '$http' , function($scope, $location, $http) {
 	var id = $location.search().id; // get url param number to fetch correct data
 	console.log(id);
-	$scope.edit = false;
+	$scope.edit = false; // close edit section
 	$scope.item = {};
 	$http.get('http://iwg-prod-web-interview.azurewebsites.net/stem/v1/funds/' + id).then(function(response) {
 		$scope.detail = [response.data];
 		console.log($scope.detail);
 	});
 
-	$scope.update = function () {
+	$scope.update = function () { // try to update the name of the fund EDIT TO DO does not allow me to edit or update name per API
 		console.log($scope.item.newName);
 		var data = {
 		  "InvestmentName": $scope.item.newName
